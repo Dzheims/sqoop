ed7d23dec0252c01eaa8f2c4a6c1828548dd7b7b
James Ivan Suminguit <jvansuminguit@gmail.com>
2021-08-21 15:27:06 +0800

    feat: add sign in function

---
+++ backend/migrations/functions/R__sign_in.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION SIGNIN(user_name TEXT, password TEXT) RETURNS jwt_token AS
+$$
+DECLARE    
+  token_information jwt_token;
+BEGIN
+    SELECT 'sqoop_user', user_id, username, extract(epoch from (now() + interval '1 week'))
+    INTO token_information
+    FROM users
+    WHERE users.username = $1 
+    AND users.hashed_password = crypt($2, users.hashed_password);
+    RETURN token_information::jwt_token;
+end;
+$$ LANGUAGE PLPGSQL VOLATILE STRICT SECURITY DEFINER;
