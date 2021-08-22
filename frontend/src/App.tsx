b0cdd5c1e5343c11c462b596cc1373cfcf830869
marsevilla2011@gmail.com <marsevilla2011@gmail.com>
2021-08-22 17:35:14 +0800

    feat: dnd columns and items frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from '@material-ui/core';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import theme from './theme';
 import Homepage from './pages/Homepage';
+import Board from './pages/Boards/Board';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <div>
         <ThemeProvider theme={theme}>
           <Switch>
-            <Route path="/" exact component={Homepage} />   
+            <Route path="/" exact component={Homepage} />
+            <Route path="/board" exact component={Board} />
           </Switch>
         </ThemeProvider>
       </div>
