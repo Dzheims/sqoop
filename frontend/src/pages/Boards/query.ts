973b9cf083b3b642ee6a2d32ff3a3baf28286dbd
marsevilla2011@gmail.com <marsevilla2011@gmail.com>
2021-08-31 20:42:14 +0800

    fix: twitter feed column

--- frontend/src/pages/Boards/query.ts
+++ frontend/src/pages/Boards/query.ts
@@ -21,8 +21,13 @@ export const GET_NEWS_API_CONTENTS_QUERY = gql`
 export const GET_TWITTER_API_CONTENTS_QUERY = gql`
   query getTwitterAPIContents {
     searchTweets {
+      author_id
+      created_at
       id
+      name
+      profile_image_url
       text
+      username
     }
   }
 `;
