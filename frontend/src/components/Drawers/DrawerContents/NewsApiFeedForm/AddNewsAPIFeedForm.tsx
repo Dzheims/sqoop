2ae90dcc2f9caa27334d82746b50a6810d85d7c2
James Ivan Suminguit <jvansuminguit@gmail.com>
2021-09-14 05:29:45 +0800

    feat: connect frontend newsapi form to backend

--- frontend/src/components/Drawers/DrawerContents/NewsApiFeedForm/AddNewsAPIFeedForm.tsx
+++ frontend/src/components/Drawers/DrawerContents/NewsApiFeedForm/AddNewsAPIFeedForm.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
 import { makeStyles } from '@material-ui/core/styles';
 import {
   Button,
@@ -9,7 +10,12 @@ import {
   Select,
   TextField,
 } from '@material-ui/core';
-import { Category } from '../../../../types.generated';
+import {
+  CreateNewsFeedMutation,
+  CreateNewsFeedMutationVariables,
+} from './query.generated';
+import { Category, CreateNewsFeedInput } from '../../../../types.generated';
+import CREATE_NEWS_FEED from './query';
 
 const useStyles = makeStyles((theme) => ({
   formContainer: {
@@ -34,6 +40,110 @@ const AddNewsAPIFeedForm = () => {
 
   const [category, setCategory] = useState('');
 
+  const [newsFeedForm, setNewsFeedForm] = useState<CreateNewsFeedInput>({
+    newsFeed: {
+      title: '',
+      category: undefined,
+      country: '',
+      keyword: '',
+      sources: '',
+    },
+  });
+
+  const onTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const { value } = e.target;
+    setNewsFeedForm({
+      ...newsFeedForm,
+      newsFeed: {
+        title: value,
+        category: newsFeedForm.newsFeed.category,
+        country: newsFeedForm.newsFeed.country,
+        keyword: newsFeedForm.newsFeed.keyword,
+        sources: newsFeedForm.newsFeed.sources,
+      },
+    });
+  };
+
+  const onKeywordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const { value } = e.target;
+    setNewsFeedForm({
+      ...newsFeedForm,
+      newsFeed: {
+        title: newsFeedForm.newsFeed.title,
+        category: newsFeedForm.newsFeed.category,
+        country: newsFeedForm.newsFeed.country,
+        keyword: value,
+        sources: newsFeedForm.newsFeed.sources,
+      },
+    });
+  };
+
+  const onSourcesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const { value } = e.target;
+    setNewsFeedForm({
+      ...newsFeedForm,
+      newsFeed: {
+        title: newsFeedForm.newsFeed.title,
+        category: newsFeedForm.newsFeed.category,
+        country: newsFeedForm.newsFeed.country,
+        keyword: newsFeedForm.newsFeed.keyword,
+        sources: value,
+      },
+    });
+  };
+
+  const onCountryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const { value } = e.target;
+    setNewsFeedForm({
+      ...newsFeedForm,
+      newsFeed: {
+        title: newsFeedForm.newsFeed.title,
+        country: value,
+        category: newsFeedForm.newsFeed.category,
+        keyword: newsFeedForm.newsFeed.keyword,
+        sources: newsFeedForm.newsFeed.sources,
+      },
+    });
+  };
+
+  const onCategoryChange = (value: Category) => {
+    setNewsFeedForm({
+      ...newsFeedForm,
+      newsFeed: {
+        title: newsFeedForm.newsFeed.title,
+        category: value,
+        keyword: newsFeedForm.newsFeed.keyword,
+        country: newsFeedForm.newsFeed.country,
+        sources: newsFeedForm.newsFeed.sources,
+      },
+    });
+  };
+
+  const [createNewsFeed] = useMutation<
+    CreateNewsFeedMutation,
+    CreateNewsFeedMutationVariables
+  >(CREATE_NEWS_FEED, {
+    variables: {
+      input: {
+        newsFeed: {
+          title: newsFeedForm.newsFeed.title,
+          category: newsFeedForm.newsFeed.category,
+          country: newsFeedForm.newsFeed.country,
+          keyword: newsFeedForm.newsFeed.keyword,
+          sources: newsFeedForm.newsFeed.sources,
+        },
+      },
+    },
+  });
+
+  const handleSubmit = () => {
+    createNewsFeed();
+  };
+
   return (
     <div className={classes.formContainer}>
       <TextField
@@ -44,23 +154,34 @@ const AddNewsAPIFeedForm = () => {
         margin="normal"
         required
         fullWidth
+        onChange={onTitleChange}
       />
       <FormControl variant="outlined" fullWidth className={classes.formControl}>
         <InputLabel>Categories</InputLabel>
         <Select
           label="Category"
           value={category}
-          // onChange={(e) => handleChange(e.target.value)}
+          onChange={(e) => onCategoryChange(e.target.value as Category)}
         >
-          <MenuItem value="Business">Business</MenuItem>
-          <MenuItem value="Entertainment">Entertainment</MenuItem>
-          <MenuItem value="General">General</MenuItem>
-          <MenuItem value="Health">Health</MenuItem>
-          <MenuItem value="Science">Science</MenuItem>
-          <MenuItem value="Sports">Sports</MenuItem>
-          <MenuItem value="Technology">Technology</MenuItem>
+          <MenuItem value={Category.Business}>Business</MenuItem>
+          <MenuItem value={Category.Entertainment}>Entertainment</MenuItem>
+          <MenuItem value={Category.General}>General</MenuItem>
+          <MenuItem value={Category.Health}>Health</MenuItem>
+          <MenuItem value={Category.Science}>Science</MenuItem>
+          <MenuItem value={Category.Sports}>Sports</MenuItem>
+          <MenuItem value={Category.Technology}>Technology</MenuItem>
         </Select>
       </FormControl>
+      <TextField
+        id="Country"
+        label="Country"
+        placeholder="Country"
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        onChange={onCountryChange}
+      />
       <TextField
         id="Keywords"
         label="Keywords"
@@ -69,6 +190,7 @@ const AddNewsAPIFeedForm = () => {
         margin="normal"
         required
         fullWidth
+        onChange={onKeywordChange}
       />
       <TextField
         id="Sources"
@@ -78,9 +200,15 @@ const AddNewsAPIFeedForm = () => {
         margin="normal"
         required
         fullWidth
+        onChange={onSourcesChange}
       />
       <div className={classes.button}>
-        <Button type="submit" variant="contained" color="secondary">
+        <Button
+          type="submit"
+          variant="contained"
+          color="secondary"
+          onClick={handleSubmit}
+        >
           Create
         </Button>
       </div>
