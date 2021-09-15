d66ef5e92babbeaa89c3100d4a544050c7f8796c
Francis Mathew Gappe <francismathew2017@gmail.com>
2021-09-15 18:09:55 +0800

    test: added create news feed mutation test

---
+++ backend/src/__tests__/newsFeeds/savingNewsFeedMutation.test.ts
@@ -0,0 +1,124 @@
+import { userSchema } from '../helpers/setupEasyGraphqlTester';
+import { gql } from 'graphile-utils';
+const EasyGraphQLTester = require('easygraphql-tester');
+
+describe('test sign in mutation', () => {
+  let tester: any;
+  beforeAll(() => {
+    tester = new EasyGraphQLTester(userSchema);
+  });
+
+  test('check if valid mutation', () => {
+    const mutation = gql`
+      mutation TESTMUTATION($newsFeed: NewsFeedInput!) {
+        createNewsFeed(input: { newsFeed: $newsFeed }) {
+          newsFeed {
+            id
+            category
+            country
+            keyword
+            sources
+            title
+          }
+        }
+      }
+    `;
+    const input = {
+      newsFeed: {
+        category: 'GENERAL',
+        country: 'us',
+        keyword: 'Covid',
+        sources: null,
+        title: 'General Feed',
+      },
+    };
+    tester.test(true, mutation, input);
+  });
+  test('check if an invalid mutation', () => {
+    const mutation = gql`
+      mutation TESTMUTATION($newsFeed: NewsFeedInput!) {
+        createNewsFeed(input: { newsFeed: $newsFeed }) {
+          newsFeed {
+            id
+            category
+            country
+            keyword
+            sources
+            title
+          }
+        }
+      }
+    `;
+    const input = {
+      newsFeed: {
+        category: 'national',
+        country: 'us',
+        keyword: 'Covid',
+        sources: null,
+        title: 'General Feed',
+      },
+    };
+    tester.test(false, mutation, input);
+  });
+});
+
+describe('mock createNewsFeed mutation', () => {
+  let tester: any;
+  beforeAll(() => {
+    tester = new EasyGraphQLTester(userSchema);
+  });
+  afterAll(() => {
+    tester.clearFixture();
+  });
+  test('return jwt token', async () => {
+    const fixture = {
+      data: {
+        createNewsFeed: {
+          newsFeed: {
+            id: 1,
+            category: 'GENERAL',
+            country: 'us',
+            keyword: 'Covid',
+            sources: null,
+            title: 'General Feed',
+          },
+        },
+      },
+    };
+    tester.setFixture(fixture);
+    const mutation = gql`
+      mutation TESTMUTATION($newsFeed: NewsFeedInput!) {
+        createNewsFeed(input: { newsFeed: $newsFeed }) {
+          newsFeed {
+            id
+            category
+            country
+            keyword
+            sources
+            title
+          }
+        }
+      }
+    `;
+    const input = {
+      newsFeed: {
+        category: 'GENERAL',
+        country: 'us',
+        keyword: 'Covid',
+        sources: null,
+        title: 'General Feed',
+      },
+    };
+    const {
+      data: {
+        createNewsFeed: { newsFeed },
+      },
+    } = await tester.mock(mutation, input);
+    expect(newsFeed.id).toBe(1);
+    expect(newsFeed.category).toBe('GENERAL');
+    expect(newsFeed.country).toBe('us');
+    expect(newsFeed.keyword).toBe('Covid');
+    expect(newsFeed.sources).toBe(null);
+    expect(newsFeed.title).toBe('General Feed');
+  });
+});
