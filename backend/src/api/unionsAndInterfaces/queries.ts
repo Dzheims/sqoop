bdb2388d4d9e395b8eebd6625a191f06f4e2e7f2
Francis Mathew Gappe <francismathew2017@gmail.com>
2021-09-19 13:59:12 +0800

    refactor: remove unused code

--- backend/src/api/unionsAndInterfaces/queries.ts
+++ backend/src/api/unionsAndInterfaces/queries.ts
@@ -11,11 +11,6 @@ interface columnProps {
 
 export const resolvers = (getNamedType: any) => {
   return {
-    Column: {
-      __resolveType({ category }: columnProps) {
-        return category ? 'NewsFeed' : 'TwitterFeed';
-      },
-    },
     ColumnResult: {
       __resolveType(column: columnProps) {
         return 'category' in column ? 'NewsFeed' : 'TwitterFeed';
@@ -32,40 +27,6 @@ export const resolvers = (getNamedType: any) => {
         );
         const result = newsFeeds.concat(twitterFeeds);
         return result;
-      },
-      getColumns: async (_: any, args: any, context: any) => {
-        const { pgClient } = context;
-        const { rows: newsFeeds } = await pgClient.query(
-          `SELECT * FROM news_feeds`
-        );
-        const { rows: twitterFeeds } = await pgClient.query(
-          `SELECT * FROM twitter_feeds`
-        );
-
-        return { ...{ newsFeeds }, ...{ twitterFeeds } };
-      },
-      getCustomNewsApiFeed: async (_: any, args: any, context: any) => {
-        const { id } = args;
-        const { pgClient } = context;
-        const {
-          rows: [filters],
-        } = await pgClient.query(
-          `SELECT category, country, keyword, sources FROM feeds WHERE id = $1`,
-          [id]
-        );
-        console.log(filters);
-        const data = await newsApiResolvers.Query.topHeadlines(
-          {},
-          {
-            category: filters.category,
-            country: filters.country,
-            sources: filters.sources,
-            keyword: filters.keyword,
-          },
-          {}
-        );
-
-        return { ...filters, ...{ data } };
       },
     },
   };
