4689ed419214496de337a7121b053fc877250f1f
James Ivan Suminguit <jvansuminguit@gmail.com>
2021-09-24 03:34:42 +0800

    fix: merge conflicts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ThemeProvider } from '@material-ui/core';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
-import Cookies from 'js-cookie';
-import AUTH_TOKEN from './constants';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import theme from './theme';
 import Homepage from './pages/Homepage';
 import SignUp from './pages/SignupPage/SignUp';
@@ -17,8 +10,6 @@ import Board from './pages/Boards/BoardsSample/Board';
 import NavigationBar from './components/Navigation/NavigationBar';
 import Columns from './components/Columns/Column';
 
-const isAuth = Cookies.get(AUTH_TOKEN);
-
 function App() {
   return (
     <Router>
@@ -27,9 +18,7 @@ function App() {
           <Switch>
             <Route path="/signup" exact component={SignUp} />
             <Route path="/signin" exact component={SignIn} />
-            {/* <Route exact path="/">
-              {isAuth ? <Homepage /> : <Redirect to="/signin" />}
-            </Route> */}
+            <Route path="/" exact component={Homepage} />
             <Route path="/column" exact component={ColumnsData} />
             <Route path="/board" exact component={Board} />
             <Route path="/column" exact component={Columns} />
