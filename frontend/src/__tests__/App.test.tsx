f5a3782e3cf10b2a55b8757b7cb502fb442e046c
James Ivan Suminguit <jvansuminguit@gmail.com>
2021-10-07 11:05:54 +0800

    fix: import error

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, RenderResult, screen } from '@testing-library/react';
 import { MockedProvider, MockedResponse } from '@apollo/client/testing';
 import App from '../App';
-import GET_COLUMNS_QUERY from '../components/Columns/query';
+import { GET_COLUMNS_QUERY } from '../components/Columns/query';
 import { ColumnsData } from '../components/Columns/ColumnsData';
 
 const mocks: ReadonlyArray<MockedResponse> = [
