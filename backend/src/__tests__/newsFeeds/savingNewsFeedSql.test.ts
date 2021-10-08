2f9b64acdf030db5d629aa0409dd41a9ea541eef
Francis Mathew Gappe <francismathew2017@gmail.com>
2021-10-08 21:30:17 +0800

    test: delete feeds sql tests

--- backend/src/__tests__/newsFeeds/savingNewsFeedSql.test.ts
+++ backend/src/__tests__/newsFeeds/savingNewsFeedSql.test.ts
@@ -17,4 +17,29 @@ describe('create news feed', () => {
       expect(news_feed.sources).toBe('cnnphilippines');
     });
   });
+  test('delete custom news feed ', async () => {
+    withRootDb(async (pgClient) => {
+      await pgClient.query<news_feeds>(
+        `INSERT INTO news_feeds (title, category, country, keyword, sources) VALUES ($1, $2, $3, $4, $5)`,
+        ['News', 'general', 'us', 'Duterte', 'cnnphilippines']
+      );
+      const {
+        rows: [news_feed],
+      } = await pgClient.query<news_feeds>(`SELECT * FROM news_feeds`);
+      expect(news_feed.keyword).toBe('Duterte');
+      expect(news_feed.title).toBe('News');
+      expect(news_feed.category).toBe('general');
+      expect(news_feed.sources).toBe('cnnphilippines');
+      const {
+        rows: [deleted_feed],
+      } = await pgClient.query<news_feeds>(
+        `DELETE FROM news_feeds WHERE id=${news_feed.id} RETURNING *`
+      );
+      expect(deleted_feed.keyword).toBe('Duterte');
+      const { rows: emptyRow } = await pgClient.query<news_feeds>(
+        `SELECT * FROM news_feeds`
+      );
+      expect(emptyRow).toBeArrayOfSize(0);
+    });
+  });
 });
--- backend/src/__tests__/twitterFeeds/savingTwitterFeedSql.test.ts
+++ backend/src/__tests__/twitterFeeds/savingTwitterFeedSql.test.ts
@@ -16,4 +16,28 @@ describe('create news feed', () => {
       expect(twitter_feed.sources).toBe('inquirerdotnet');
     });
   });
+  test('delete custom news feed ', async () => {
+    withRootDb(async (pgClient) => {
+      await pgClient.query<twitter_feeds>(
+        `INSERT INTO news_feeds (title, keyword, sources) VALUES ($1, $2, $3)`,
+        ['Twitter', 'Marcos', 'inquirerdotnet']
+      );
+      const {
+        rows: [news_feed],
+      } = await pgClient.query<twitter_feeds>(`SELECT * FROM news_feeds`);
+      expect(news_feed.keyword).toBe('Marcos');
+      expect(news_feed.title).toBe('Twitter');
+      expect(news_feed.sources).toBe('inquirerdotnet');
+      const {
+        rows: [deleted_feed],
+      } = await pgClient.query<twitter_feeds>(
+        `DELETE FROM news_feeds WHERE id=${news_feed.id} RETURNING *`
+      );
+      expect(deleted_feed.keyword).toBe('Marcos');
+      const { rows: emptyRow } = await pgClient.query<twitter_feeds>(
+        `SELECT * FROM news_feeds`
+      );
+      expect(emptyRow).toBeArrayOfSize(0);
+    });
+  });
 });
