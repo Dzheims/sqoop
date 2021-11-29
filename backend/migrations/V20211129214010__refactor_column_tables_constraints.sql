5bf8ce8f3cbdd8b67887e894cd815ddfdff0b0d0
James Ivan Suminguit <jvansuminguit@gmail.com>
2021-11-29 22:03:21 +0800

    fix: fix column tables constraints

---
+++ backend/migrations/V20211129214010__refactor_column_tables_constraints.sql
@@ -0,0 +1,18 @@
+ALTER TABLE collections
+    DROP CONSTRAINT collection_unique_title;
+
+ALTER TABLE news_feeds
+    DROP CONSTRAINT news_feed_unique_title;
+
+ALTER TABLE twitter_feeds
+    DROP CONSTRAINT twitter_feed_unique_title;
+
+ALTER TABLE collections
+    ADD CONSTRAINT collection_unique_title UNIQUE (title, id);
+
+ALTER TABLE news_feeds
+    ADD CONSTRAINT news_feed_unique_title UNIQUE (title, id);
+
+ALTER TABLE twitter_feeds
+    ADD CONSTRAINT twitter_feed_unique_title UNIQUE (title, id);
+
