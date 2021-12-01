e3e9728755b7404cae6cb945420806454e3897f6
Marsry Jan Sevilla <marsevilla2011@gmail.com>
2021-12-01 12:40:55 +0800

    test: google fact check and vera files cards

---
+++ frontend/src/__tests__/GoogleFactCheckCards.test.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { render, RenderResult } from '@testing-library/react';
+import { MockedProvider } from '@apollo/client/testing';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import GoogleFactCheckResultsCards from '../components/FactCheck/GoogleFactCheckResultsCards';
+import { Claim } from '../types.generated';
+
+const data: Claim = {
+  text: 'The World Econimic Forum reported on the Omicron COVID-19 variant in July.',
+  claimant: 'Facebook,Twitter',
+  claimDate: '2021-10-12T10:30:47.000Z',
+  publisherName: 'The Quint',
+  publisherSite: 'thequint.com',
+  url: 'https://www.thequint.com/news/webqoof/world-economic-forum-reported-on-omicron-in-july-fact-check',
+  title: "No, World Economic Forum Didn't Report on Omicron COVID-19 ...",
+  reviewDate: '2021-11-30T10:49:17Z',
+  textualRating: 'False',
+  languageCode: 'en',
+};
+
+let documentBody: RenderResult;
+
+describe('Google Fact Check Cards', () => {
+  beforeEach(() => {
+    documentBody = render(
+      <MockedProvider mocks={[]} addTypename={false}>
+        <DragDropContext onDragEnd={() => {}}>
+          <Droppable droppableId="droppable">
+            {(provided) => (
+              <div ref={provided.innerRef}>
+                <GoogleFactCheckResultsCards data={data} />
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </MockedProvider>
+    );
+  });
+  test('check claim text', async () => {
+    const text = await documentBody.findByText(
+      'The World Econimic Forum reported on the Omicron COVID-19 variant in July.'
+    );
+    expect(text).toBeInTheDocument();
+  });
+  test('check claim title', async () => {
+    const text = await documentBody.findByText(
+      "No, World Economic Forum Didn't Report on Omicron COVID-19 ..."
+    );
+    expect(text).toBeInTheDocument();
+  });
+  test('check claimant', async () => {
+    const text = await documentBody.findByText('Claim by Facebook,Twitter');
+    expect(text).toBeInTheDocument();
+  });
+  test('check publisher name and textual rating', async () => {
+    const text = await documentBody.findByText('The Quint Info Rating: False');
+    expect(text).toBeInTheDocument();
+  });
+  test('check claim date', async () => {
+    const text = await documentBody.findByText(
+      'Claim Date 10:30:47 AM Tue Oct 12 2021'
+    );
+    expect(text).toBeInTheDocument();
+  });
+  test('check review date', async () => {
+    const text = await documentBody.findByText(
+      'Review Date 10:49:17 AM Tue Nov 30 2021'
+    );
+    expect(text).toBeInTheDocument();
+  });
+});
