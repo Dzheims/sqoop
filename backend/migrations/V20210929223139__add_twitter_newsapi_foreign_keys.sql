1d8c04588d8417348f03c0cc8b403d01b32a21f2
James Ivan Suminguit <jvansuminguit@gmail.com>
2021-12-07 23:52:55 +0800

    fix: fix duplicate column title

--- backend/migrations/V20210929223139__add_twitter_newsapi_foreign_keys.sql
+++ backend/migrations/V20210929223139__add_twitter_newsapi_foreign_keys.sql
@@ -1,6 +1,10 @@
-ALTER TABLE twitter_feeds ADD user_id int REFERENCES users(user_id);
-ALTER TABLE news_feeds ADD user_id int REFERENCES users(user_id);
+ALTER TABLE twitter_feeds
+    ADD user_id int REFERENCES users (user_id);
 
-CREATE INDEX ON twitter_feeds(user_id);
-CREATE INDEX ON news_feeds(user_id);
+ALTER TABLE news_feeds
+    ADD user_id int REFERENCES users (user_id);
+
+CREATE INDEX ON twitter_feeds (user_id);
+
+CREATE INDEX ON news_feeds (user_id);
 
