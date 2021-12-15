41fbb4593e2306a6349e323250c4468f81729b7e
James Ivan Suminguit <jvansuminguit@gmail.com>
2021-12-15 22:15:21 +0800

    feat: default page for 404 error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/core';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import Cookies from 'js-cookie';
 import theme from './theme';
 import Homepage from './pages/Homepage';
 import SignUp from './pages/SignupPage/SignUp';
@@ -9,6 +15,7 @@ import { ColumnsData } from './components/Columns/ColumnsData';
 import Board from './pages/Boards/BoardsSample/Board';
 import NavigationBar from './components/Navigation/NavigationBar';
 import Columns from './components/Columns/Column';
+import AUTH_TOKEN from './constants';
 
 function App() {
   return (
@@ -18,7 +25,15 @@ function App() {
           <Switch>
             <Route path="/signup" exact component={SignUp} />
             <Route path="/signin" exact component={SignIn} />
-            <Route path="/" exact component={Homepage} />
+            <Route path="/home" exact component={Homepage} />
+            <Route
+              render={() => {
+                if (Cookies.get(AUTH_TOKEN)) {
+                  return <Redirect to="/home" />;
+                }
+                return <Redirect to="/" />;
+              }}
+            />
           </Switch>
         </ThemeProvider>
       </div>
